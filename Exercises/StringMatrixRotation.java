7bad2d5b3f2887674eee75f213a6299b338a12ad
HristoNakov13 <admiralbiat@gmail.com>
2019-05-17 17:16:32 +0300

    Update StringMatrixRotation.java

--- Exercises/StringMatrixRotation.java
+++ Exercises/StringMatrixRotation.java
@@ -1,115 +1,97 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 
 public class StringMatrixRotation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
 
-        Pattern pattern = Pattern.compile("[A-Za-z]+\\((?<degree>\\d+)\\)");
-        Matcher matcher = pattern.matcher(scanner.nextLine());
-
-        int degrees;
-
-        if (matcher.find()) {
-            degrees = Integer.parseInt(matcher.group("degree")) % 360;
-        }else {
-            return;
-        }
-
-
-        List<String> words = new ArrayList<>();
+        int rotation = Integer.parseInt(buffer.readLine().replaceAll("[A-Za-z()]+", "")) % 360;
         String input;
-        int cols = -1;
-        int rows = 0;
+        int maxLength = -1;
+        List<String> inputLines = new ArrayList<>();
 
+        while (!"END".equals(input = buffer.readLine())) {
 
-        while (!"END".equals(input = scanner.nextLine())) {
-
-            rows++;
-            words.add(input);
-
-            if (input.length() > cols) {
-                cols = input.length();
+            inputLines.add(input);
+            if (input.length() > maxLength) {
+                maxLength = input.length();
             }
         }
+        char[][] matrix = new char[inputLines.size()][maxLength];
 
-        String[][] matrix = new String[rows][cols];
-
-        for (int i = 0; i < rows; i++) {
-
-            for (int j = 0; j < cols; j++) {
-
-                if (j < words.get(i).length()) {
-                    matrix[i][j] = Character.toString(words.get(i).charAt(j));
+        for (int i = 0; i < inputLines.size(); i++) {
+            for (int j = 0; j < maxLength; j++) {
+                if (j < inputLines.get(i).length()) {
+                    matrix[i][j] = inputLines.get(i).charAt(j);
                 }else {
-                    matrix[i][j] = " ";
+                    matrix[i][j] = ' ';
                 }
             }
-
         }
-        int temp = rows;
+        while (!"END".equals(input = buffer.readLine())) {
 
-        if (degrees == 90 || degrees == 270) {
-            rows = cols;
-            cols = temp;
-        }
-        String[][] rotated = new String[rows][cols];
-        System.out.println(print(matrix, degrees));
+            matrix = matrixRotation(matrix, rotation);
+            for (char[] chars : matrix) {
 
-    }
-
-    private static String print (String[][] matrix, int degrees) {
-
-        StringBuilder print = new StringBuilder();
-
-        switch (degrees) {
-            case 0:
-                for (int i = 0; i < matrix.length; i++) {
-
-                    for (int j = 0; j < matrix[0].length; j++) {
-                        print.append(matrix[i][j]);
-                    }
-                    print.append("\n");
-
+                for (int j = 0; j < matrix[0].length; j++) {
+                    System.out.print(chars[j]);
                 }
-                break;
-            case 90:
-                for (int i = 0; i < matrix[0].length; i++) {
+                System.out.println();
+            }
+            rotation = Integer.parseInt(input);
+        }
+    }
+    private static char[][] matrixRotation(char[][] matrix, int rotation) {
 
-                    for (int j = 0; j < matrix.length; j++) {
-                        print.append(matrix[matrix.length - 1 - j][i]);
+        char[][] wtf;
+        int rows, cols;
+
+        switch (rotation) {
+            case 90:
+                wtf = new char[matrix[0].length][matrix.length];
+                rows = wtf.length;
+                cols = wtf[0].length;
+
+                for (int i = 0; i < rows; i++) {
+
+                    for (int j = 0; j < cols; j++) {
+
+                        wtf[i][j] = matrix[wtf[0].length - 1 - j][i];
                     }
-                    print.append("\n");
                 }
                 break;
             case 180:
-                for (int i = matrix.length - 1; i >= 0 ; i--) {
+                wtf = new char[matrix.length][matrix[0].length];
+                rows = wtf.length;
+                cols = wtf[0].length;
 
-                    for (int j = matrix[0].length - 1; j >= 0 ; j--) {
-                        print.append(matrix[i][j]);
+                for (int i = 0; i < rows; i++) {
 
+                    for (int j = 0; j < cols; j++) {
+                        wtf[i][j] = matrix[matrix.length - 1 - i][matrix[0].length - 1 - j];
                     }
-                    print.append("\n");
                 }
                 break;
             case 270:
-                for (int i = matrix[0].length - 1; i >= 0; i--) {
+                wtf = new char[matrix[0].length][matrix.length];
+                rows = wtf.length;
+                cols = wtf[0].length;
 
-                    for (int j = 0; j < matrix.length; j++) {
+                for (int i = 0; i < rows; i++) {
 
-                        print.append(matrix[j][i]);
+                    for (int j = 0; j < cols; j++) {
+                        wtf[i][j] = matrix[j][matrix[0].length - 1 - i];
                     }
-                    print.append("\n");
                 }
-
+                break;
+            default:
+                wtf = matrix;
                 break;
         }
-
-        return print.toString();
+        return wtf;
     }
 }
