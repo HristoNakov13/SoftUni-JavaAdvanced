b2d759237f1ca8e4336ec0289bbd81d1377b87b8
HristoNakov13 <admiralbiat@gmail.com>
2019-05-21 15:29:00 +0300

    10. *Population Counter - Sets and Maps
    So many people! It’s hard to count them all. But that’s your job as a statistician. You get raw data for a given city and you need to aggregate it. 
    On each input line you’ll be given data in format: "city|country|population". There will be no redundant whitespaces anywhere in the input. Aggregate the data by country and by city and print it on the console. For each country, print its total population and on separate lines the data for each of its cities. Countries should be ordered by their total population in descending order and within each country, the cities should be ordered by the same criterion. If two countries/cities have the same population, keep them in the order in which they were entered. Check out the examples; follow the output format strictly!
    input:
    Sofia|Bulgaria|1
    Veliko Tarnovo|Bulgaria|2
    London|UK|4
    Rome|Italy|3
    report

---
+++ Exercises/PopulationCounter.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PopulationCounter {
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        LinkedHashMap<String, LinkedHashMap<String, Integer>> populationStats = new LinkedHashMap<>();
+        String input;
+
+        while (!"report".equals(input = scanner.nextLine())) {
+
+            String[] lineData = input.split("\\|");
+            String country = lineData[1];
+            String city = lineData[0];
+            int population = Integer.parseInt(lineData[2]);
+
+            populationStats.putIfAbsent(country, new LinkedHashMap<>());
+            populationStats.get(country).put(city, population);
+        }
+        ;
+        Map<String, Long> totalPopulationByCountry = new HashMap<>();
+
+        for (Map.Entry<String, LinkedHashMap<String, Integer>> country : populationStats.entrySet()) {
+            long popSum = 0;
+            for (Map.Entry<String, Integer> city : country.getValue().entrySet()) {
+                popSum += city.getValue();
+            }
+            totalPopulationByCountry.put(country.getKey(), popSum);
+        }
+
+        populationStats.entrySet().stream()
+                .sorted((country1, country2) ->
+                        Long.compare(totalPopulationByCountry.get(country2.getKey()),
+                                totalPopulationByCountry.get(country1.getKey())))
+                .forEach(country -> {
+
+                    System.out.println(String.format("%s (total population: %d)"
+                            , country.getKey(), totalPopulationByCountry.get(country.getKey())));
+
+                    country.getValue().entrySet().stream()
+                            .sorted((city1, city2) -> Integer.compare(city2.getValue(), city1.getValue()))
+                            .forEach(city -> System.out.println(String.format("=>%s: %d"
+                                    , city.getKey(), city.getValue())));
+                });
+    }
+}
